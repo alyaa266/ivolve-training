68d5140b15d6ddfff5b0f0cce8dd988c37d5b919
alyaa266 <84872262+alyaa266@users.noreply.github.com>
2024-06-04 00:11:30 +0300

    Delete terraform directory

--- terraform/terraformmodule lab2/modules/RDS/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "db_user" {
-  description = "Username for the RDS instance"
-  default     = "admin" // Example RDS username
-}
-
-variable "db_pass" {
-  description = "Password for the RDS instance"
-  default     = "PssW0rd_2024!" // Example RDS password
-}
--- terraform/terraformmodule lab2/modules/network/vars.tf
+++
@@ -1,4 +0,0 @@
-variable "ami" {
-  description = "The AMI ID for the EC2 instance"
-  default     = "ami-0d3a2960fcac852bc" // Example AMI ID
-}

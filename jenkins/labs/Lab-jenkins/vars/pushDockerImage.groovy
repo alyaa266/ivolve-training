c1e0a62725060322da4996430389c233691b65f0
alyaa266 <84872262+alyaa266@users.noreply.github.com>
2024-06-06 12:07:48 +0300

    Delete jenkins/labs directory

--- jenkins/labs/Lab-jenkins/vars/pushDockerImage.groovy
+++
@@ -1,6 +0,0 @@
-def call(String imageName, String tag, String credentialsId) {
-    docker.withRegistry('https://index.docker.io/v1/', credentialsId) {
-        docker.image("${imageName}:${tag}").push()
-    }
-}
-
